2f89507d12c99e7023ce11456c4ae0965279d2e3
Dirk Page <dpage@eco.com>
2025-01-23 23:17:37 -0600

    Testing invalidness in IntentsService unit tests

--- packages/sdk/test/unit/IntentsService.test.ts
+++ packages/sdk/test/unit/IntentsService.test.ts
@@ -1,9 +1,9 @@
-import { describe, test, expect, beforeAll } from "vitest";
-import { encodeFunctionData, erc20Abi, keccak256 } from "viem";
+import { describe, test, expect, beforeAll, beforeEach } from "vitest";
+import { encodeFunctionData, erc20Abi, Hex, keccak256 } from "viem";
 
 import { EcoProtocolAddresses } from "@eco-foundation/routes";
 
-import { RoutesService, IntentsService, SolverQuote } from "../../src";
+import { RoutesService, IntentsService, SolverQuote, IntentData } from "../../src";
 import { dateToTimestamp, getSecondsFromNow } from "../../src/utils";
 import { encodeRoute, encodeReward, encodeIntent } from "../../src/intents/utils";
 
@@ -11,37 +11,45 @@ describe("IntentsService", () => {
   let routesService: RoutesService;
   let intentsService: IntentsService;
 
-  beforeAll(async () => {
+  let validIntentData: IntentData;
+  let validQuote: SolverQuote;
+  let creator: Hex;
+
+  beforeAll(() => {
     routesService = new RoutesService();
     intentsService = new IntentsService();
+    validIntentData = routesService.createSimpleRoute({
+      originChainID: 10,
+      destinationChainID: 8453,
+      spendingToken: RoutesService.getTokenAddress(10, "USDC"),
+      receivingToken: RoutesService.getTokenAddress(8453, "USDC"),
+      amount: BigInt(1000000),
+      prover: 'HyperProver',
+      simpleRouteActionData: encodeFunctionData({
+        abi: erc20Abi,
+        functionName: 'transfer',
+        args: ['0xe494e1285d741F90b4BA51482fa7c1031B2DD294', BigInt(1000000)]
+      })
+    });
+    creator = "0xe494e1285d741F90b4BA51482fa7c1031B2DD294"
+  })
+
+  beforeEach(() => {
+    validQuote = {
+      receiveSignedIntentUrl: "https://example.com/endpoint",
+      intentSourceContract: EcoProtocolAddresses[10].IntentSource,
+      quoteData: {
+        rewardTokens: [RoutesService.getTokenAddress(10, "USDC")],
+        rewardTokenAmounts: ["1000000"],
+        expiryTime: dateToTimestamp(getSecondsFromNow(60)).toString()
+      }
+    };
   });
 
   describe("setupIntentForPublishing", () => {
     test("valid", async () => {
-      const creator = "0xe494e1285d741F90b4BA51482fa7c1031B2DD294";
-      const intentData = routesService.createSimpleRoute({
-        originChainID: 10,
-        destinationChainID: 8453,
-        spendingToken: RoutesService.getTokenAddress(10, "USDC"),
-        receivingToken: RoutesService.getTokenAddress(8453, "USDC"),
-        amount: BigInt(1000000),
-        prover: 'HyperProver',
-        simpleRouteActionData: encodeFunctionData({
-          abi: erc20Abi,
-          functionName: 'transfer',
-          args: ['0xe494e1285d741F90b4BA51482fa7c1031B2DD294', BigInt(1000000)]
-        })
-      });
-
-      const quote: SolverQuote = {
-        receiveSignedIntentUrl: "https://example.com",
-        intentSourceContract: EcoProtocolAddresses[10].IntentSource,
-        quoteData: {
-          rewardTokens: [RoutesService.getTokenAddress(10, "USDC")],
-          rewardTokenAmounts: ["1000000"],
-          expiryTime: dateToTimestamp(getSecondsFromNow(60)).toString()
-        }
-      };
+      const intentData = validIntentData;
+      const quote = validQuote;
 
       const { intent, intentHash, routeHash, rewardHash } = intentsService.setupIntentForPublishing({ creator, intentData, quote });
 
@@ -55,5 +63,39 @@ describe("IntentsService", () => {
       expect(keccak256(encodeReward(intent.reward))).toBe(rewardHash);
       expect(keccak256(encodeIntent(intent))).toBe(intentHash);
     });
+
+    test("invalid creator address", () => {
+      creator = "0x";
+      const intentData = validIntentData;
+      const quote = validQuote;
+
+      expect(() => intentsService.setupIntentForPublishing({ creator, intentData, quote })).toThrow(`Address "0x" is invalid.`);
+    });
+
+    test("invalid intent data", async () => {
+      const creator = "0xe494e1285d741F90b4BA51482fa7c1031B2DD294";
+      const intentData: IntentData = {
+        ...validIntentData,
+        destinationChainActions: []
+      };
+      const quote = validQuote;
+
+      expect(() => intentsService.setupIntentForPublishing({ creator, intentData, quote })).toThrow(`Cannot read properties of undefined (reading 'length')`);
+    });
+
+    test("invalid quote data", async () => {
+      const creator = "0xe494e1285d741F90b4BA51482fa7c1031B2DD294";
+      const intentData = validIntentData;
+      const quote: SolverQuote = {
+        ...validQuote,
+        quoteData: {
+          ...validQuote.quoteData,
+          rewardTokenAmounts: [],
+        }
+      };
+
+      expect(() => intentsService.setupIntentForPublishing({ creator, intentData, quote })).toThrow("Cannot convert undefined to a BigInt");
+    });
+
   });
 });
