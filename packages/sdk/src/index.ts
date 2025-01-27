6158bf48424e4c614a86e93cdb16e9ec5f45afe7
Dirk <dpage@eco.com>
2025-01-27 14:07:17 -0600

    Update file names for services and clients and updated imports (#12)

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,7 +1,7 @@
-export { RoutesService } from "./routes";
+export { RoutesService } from "./routes/RoutesService";
 export type { CreateSimpleRouteParams, CreateRouteParams, SetupIntentForPublishingParams, IntentData } from "./routes/types";
 
-export { OpenQuotingClient } from "./quotes";
+export { OpenQuotingClient } from "./quotes/OpenQuotingClient";
 export type { SolverQuote, QuoteData } from "./quotes/types";
 export { selectCheapestQuote } from "./quotes/quoteSelectors";
 
