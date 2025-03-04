285aff2424082ea4f8e4ea6cf7df58ef52447cbf
Dirk Page <dpage@eco.com>
2025-03-04 12:44:09 -0600

    Adding polygon USDCe to supported stables

--- packages/sdk/src/constants/index.ts
+++ packages/sdk/src/constants/index.ts
@@ -26,6 +26,7 @@ export const stableAddresses: Record<RoutesSupportedChainId, Partial<Record<Rout
   },
   137: {
     USDC: "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359",
+    USDCe: "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174",
     USDT: "0xc2132d05d31c914a87c6611c10748aeb04b58e8f",
   },
   // 2741: {
