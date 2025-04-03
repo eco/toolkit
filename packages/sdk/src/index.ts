e6518282106120e1a6dead9afd7febf9a925a1bc
Dirk Page <dpage@eco.com>
2025-04-02 23:20:29 -0500

    Updated exported types

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -2,8 +2,8 @@ export { INTENT_EXECUTION_TYPES, chainIds, stables, stableAddresses } from "./co
 export type { IntentExecutionType, RoutesSupportedChainId, RoutesSupportedStable } from "./constants";
 
 export { RoutesService } from "./routes/RoutesService";
-export type { CreateSimpleIntentParams, CreateIntentParams, ApplyQuoteToIntentParams } from "./routes/types";
+export type { CreateSimpleIntentParams, CreateIntentParams } from "./routes/types";
 
 export { OpenQuotingClient } from "./quotes/OpenQuotingClient";
-export type { RequestQuotesForIntentParams, SolverQuote, QuoteData } from "./quotes/types";
+export type { RequestQuotesForIntentParams, SolverQuote, QuoteData, PermitData, Permit1, Permit2, SinglePermit2Data, BatchPermit2Data, Permit2DataDetails, InitiateGaslessIntentResponse } from "./quotes/types";
 export { selectCheapestQuote } from "./quotes/quoteSelectors";
