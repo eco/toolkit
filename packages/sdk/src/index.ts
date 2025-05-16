96662c3edb843c7c966029880a2edb1221d23d39
Dirk Page <dpage@eco.com>
2025-05-16 14:45:48 -0500

    Removing getFee

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -3,7 +3,6 @@ export type { IntentExecutionType, RoutesSupportedChainId, RoutesSupportedStable
 
 export { RoutesService } from "./routes/RoutesService";
 export type { CreateSimpleIntentParams, CreateIntentParams } from "./routes/types";
-export { getFee } from "./routes/getFee";
 
 export { OpenQuotingClient } from "./quotes/OpenQuotingClient";
 export type { RequestQuotesForIntentParams, InitiateGaslessIntentParams, SolverQuote, QuoteData, QuoteSelectorResult, PermitData, Permit1, Permit2, SinglePermit2Data, BatchPermit2Data, Permit2DataDetails, InitiateGaslessIntentResponse } from "./quotes/types";
