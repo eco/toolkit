402dbcbf601d6f0c81b9a1e55614018d409f0119
Dirk Page <dpage@eco.com>
2025-05-23 15:05:16 -0500

    Updating dependencies

--- apps/sdk-demo/src/wagmi.ts
+++ apps/sdk-demo/src/wagmi.ts
@@ -1,6 +1,5 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import { EcoChains } from '@eco-foundation/chains';
-import { http } from 'wagmi';
 
 if (!process.env.NEXT_PUBLIC_WALLET_CONNECT_ID) {
   throw new Error('NEXT_PUBLIC_WALLET_CONNECT_ID is required');
